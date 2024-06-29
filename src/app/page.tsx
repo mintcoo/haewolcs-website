7adfe348869eccdc57c65177e1b91b6f524b66cb
HanSangHyun <xhaktm3344@gmail.com>
2024-06-29 17:48:25 +0900

    Eslint 및 Prettier 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Navbar from "@/components/navbar";
+
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-	);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-200">
+      <Navbar />
+    </main>
+  );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  // 현재 나의 페이지 url정보 가져오자
+//   const path = usePathname();
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/about-us"}>About Us</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
