c12c47c1dcabb844cc833db8461548f33d99d39a
mintcoo <109326433+mintcoo@users.noreply.github.com>
2024-07-04 00:00:50 +0900

    "use client" 세팅

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   // 현재 나의 페이지 url정보 가져오자
-//   const path = usePathname();
+  const path = usePathname();
 
   return (
     <nav>
