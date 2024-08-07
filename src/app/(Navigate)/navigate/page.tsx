fba150ca9571283894e2018feeee377366f5ac88
mintcoo <109326433+mintcoo@users.noreply.github.com>
2024-08-08 01:45:44 +0900

    오시는길 추가중

--- src/app/(Navigate)/navigate/page.tsx
+++ src/app/(Navigate)/navigate/page.tsx
@@ -1,5 +1,10 @@
 import NaverMap from "@/components/Home/NaverMap";
-import { CallOutlined, EventNote, PinDropOutlined } from "@mui/icons-material";
+import {
+  CallOutlined,
+  EventNote,
+  PinDropOutlined,
+  TrainOutlined,
+} from "@mui/icons-material";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,27 +18,39 @@ export default function Navigate() {
       <div className="sub-title-bar"></div>
       <div className="w-full f-c-c-c">
         <NaverMap />
-        <div className="w-full border-t-2 border-b-2 py-4 f-c-c-c my-10 gap-4">
+        <div className="w-full border-t-2 border-b-2 py-4 f-c-c-c my-10 gap-5">
           <div className="flex font-semibold text-xl">
             <PinDropOutlined fontSize="large" color="primary" />
-            <div className="w-40 mx-10">주소</div>
-            <div className="w-96">부산 해운대구 달맞이길 227(중동 1480-2)</div>
+            <p className="w-40 mx-10 align-bottom ">주소</p>
+            <div className="w-96 font-normal">
+              부산 해운대구 달맞이길 227(중동 1480-2)
+            </div>
           </div>
           <div className="flex font-semibold text-xl">
             <CallOutlined fontSize="large" color="primary" />
             <div className="w-40 mx-10">대표번호</div>
-            <div className="w-96">051)710-7090</div>
+            <div className="w-96 font-normal">051)710-7090</div>
           </div>
           <div className="flex font-semibold text-xl">
             <EventNote fontSize="large" color="primary" />
-            <div className="w-40 mx-10">진료</div>
-            <div className="w-96">
-              <div>평일</div>
-              <div>09:00 - 17:00</div>
-              <div>토요일</div>
-              <div>09:00 - 17:00</div>
-              <div>점심시간</div>
-              <div>12:30 - 13:30</div>
+            <div className="w-40 mx-10">진료시간</div>
+            <div className="w-96 text-lg">
+              <div className="text-xl">평일</div>
+              <div className="pl-8 font-normal">09:00 - 17:00</div>
+              <div className="text-xl">토요일</div>
+              <div className="pl-8 font-normal">09:00 - 17:00</div>
+              <div className="text-xl">점심시간</div>
+              <div className="pl-8 font-normal">12:30 - 13:30</div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full border-b-2 f-c-c-c">
+          <div className="flex font-semibold text-xl">
+            <TrainOutlined fontSize="large" color="primary" />
+            <div className="w-40 mx-10">대중교통</div>
+            <div className="w-96 text-lg font-normal">
+              장산역 6번출구 마을버스{" "}
+              <span className="font-bold">2번, 7번</span>
             </div>
           </div>
         </div>
