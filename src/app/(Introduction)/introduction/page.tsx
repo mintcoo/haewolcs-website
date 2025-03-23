e0cecf3e93c63b63967720345f2b90c4e55eaf1b
HanSangHyun <xhaktm3344@gmail.com>
2025-03-23 15:58:48 +0900

    의원 소개 강조구문들 표기

--- src/app/(Introduction)/introduction/page.tsx
+++ src/app/(Introduction)/introduction/page.tsx
@@ -17,7 +17,7 @@ export default function Introduction() {
           <div className="text-lg sm:text-xl md:text-2xl haewol-darkblue-title">
             부산 해운대구 달맞이길
           </div>
-          <span className="font-bold text-lg sm:text-xl md:text-2xl haewol-darkblue-title">
+          <span className="font-semibold text-lg sm:text-xl md:text-2xl haewol-darkblue-title">
             해월씨에스 의원 원장 한창순
           </span>
           <span className="text-lg sm:text-xl md:text-2xl haewol-darkblue-title">
@@ -32,33 +32,88 @@ export default function Introduction() {
 1층 주차장 위로 3,4 층은 병실, 간호사실, 주사실, 물리치료실이 있고, 
 5층은 원무과, 진료실, 고주파온열치료실, 1인실, 주방과 식당이 있습니다.
 병실은 `}
-            <strong className="haewol-darkblue-title font-bold">
+            <strong className="haewol-darkblue-title font-semibold">
               4인실 (5실), 3인실 (2실), 2인실 (1실)과 1인실 (1실){" "}
             </strong>
             이 있으며,
             <br />
-            {`수술 후, 항암 치료, 방사선 치료 전후에 장기 및 단기 입원과 외래 치료가 가능하며, 
+            {`수술 후, 항암 치료, 방사선 치료 전후에 `}
+            <strong className="haewol-darkblue-title font-semibold">
+              장기 및 단기 입원과 외래 치료가 가능하며,
+            </strong>
+            {`
 항암 치료 부작용 관리와 
 아래 열거한 암 통합치료를 받으실 수 있습니다.`}
           </p>
           <br />
           <p className="whitespace-pre-line">
-            {`온코써미아를 이용한 고주파 온열치료, 
-면역증강치료로 싸이모신 알파원 (헤리, 자닥신, 휴닥신) 주사 및 
-미슬토제제로 압노바, 이스카도, 헬릭소 주사,
-면역항암제로 이뮤코텔, 메시마, 
-녹십자와 연계하여 이뮨셀 주사,
-항산화치료로 셀레늄, 글루타치온, 치옥트산 주사 및 ​
-암에 대한 직접 치료로 고용량 비타민 C 요법 등을 시행하며, 
+            {`온코써미아를 이용한`}{" "}
+            <strong className="haewol-orange-color font-semibold">
+              고주파 온열치료
+            </strong>
+            {`, 
+면역증강치료로 `}{" "}
+            <strong className="haewol-darkblue-title font-semibold">
+              싸이모신 알파원
+            </strong>{" "}
+            <strong className="haewol-orange-color font-semibold">
+              (헤리, 자닥신, 휴닥신)
+            </strong>
+            {`  주사 및`}
+            <br />
+            <strong className="haewol-darkblue-title font-semibold">
+              미슬토제제
+            </strong>
+            {`로 `}
+            <strong className="haewol-orange-color font-semibold">
+              압노바, 이스카도, 헬릭소
+            </strong>
+            {` 주사,
+면역항암제로 `}
+            <strong className="haewol-orange-color font-semibold">
+              이뮤코텔, 메시마,
+            </strong>
+            {`
+녹십자와 연계하여 `}
+            <strong className="haewol-orange-color font-semibold">
+              이뮨셀
+            </strong>
+            {` 주사,
+항산화치료로 `}
+            <strong className="haewol-orange-color font-semibold">
+              셀레늄, 글루타치온, 치옥트산
+            </strong>
+            {` 주사 및 ​
+암에 대한 직접 치료로 `}
+            <strong className="haewol-orange-color font-semibold">
+              고용량 비타민 C 요법
+            </strong>
+            {` 등을 시행하며, 
 이에 대한 자세한 설명은 “암 통합치료” 난에 기재되어 있습니다.
-그 외에 비타민 D, 마이어스 칵테일, 푸르설티아민, 징크 등 미네랄과 비타민 공급, 
+그 외에 `}
+            <strong className="haewol-orange-color font-semibold">
+              비타민 D, 마이어스 칵테일, 푸르설티아민, 징크
+            </strong>
+            {` 등 미네랄과 비타민 공급, 
 아미노산, 콤비플렉스 등의 영양 수액제 처방이 가능합니다.`}
           </p>
           <br />
           <p className="whitespace-pre-line">
-            {`고주파온열치료 기계는 Oncothermia EHY-2000 PLUS를 도입하였으며 
-모든 주사제의 조제는 무균조제 부스에서 시행하고 있습니다.
- 온열 물침대가 구비되어 있으며, 
+            {`고주파온열치료 기계는 `}
+            <strong className="haewol-darkblue-title font-semibold">
+              Oncothermia EHY-2000 PLUS
+            </strong>
+            {`를 도입하였으며 
+모든 주사제의 조제는 `}
+            <strong className="haewol-darkblue-title font-semibold">
+              무균조제 부스
+            </strong>
+            {`에서 시행하고 있습니다.
+ `}
+            <strong className="haewol-darkblue-title font-semibold">
+              온열 물침대
+            </strong>
+            {`가 구비되어 있으며, 
 반신욕기와 런닝머신은 바다가 보이는 베란다에 설치되어 있어 
 달맞이길의 문탠로드를 따라 펼쳐져 있는 소나무 숲과 
 그 너머로 보이는 바다를 보며 휴식과 안정을 취할 수 있습니다. 
