4d8a1ac384bcc9905564a90f7e41bdff80febcd5
HanSangHyun <xhaktm3344@gmail.com>
2025-04-07 15:28:50 +0900

    환우들과 함께 페이지 일반 검색용 description 추가

--- src/app/(Pages)/(Story)/story/layout.tsx
+++ src/app/(Pages)/(Story)/story/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "환우들과 함께",
+  description: "해월씨에스 의원에서 환우들과 함께하는 이야기를 나눕니다.",
   openGraph: {
     title: "환우들과 함께",
     description: "해월씨에스 의원에서 환우들과 함께하는 이야기를 나눕니다.",
